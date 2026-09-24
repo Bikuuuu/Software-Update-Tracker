df20f7dd68c79e2f7c9865d2b06ce3e9d35a8a00
Bikuuuu <169059603+Bikuuuu@users.noreply.github.com>
2026-09-25 00:52:14 +0400

    Add solution skeleton

---
+++ src/SoftwareUpdateTracker.Core/AppInfo.cs
@@ -0,0 +1,7 @@
+namespace SoftwareUpdateTracker.Core;
+
+public static class AppInfo
+{
+    public const string Name = "Software Update Tracker";
+    public const string InstanceKey = "SoftwareUpdateTracker";
+}
